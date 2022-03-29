f0f7023134ef848039e029149d498351ab93ac28
Timothy White <timothy.white@eagles.oc.edu>
2022-03-29 15:38:28 -0500

    Edit from /user

--- routes/user.js
+++ routes/user.js
@@ -21,4 +21,26 @@ router.post('/', [authenticate, isNewUser], function(req, res, next) {
 	});
 });
 
+router.put('/', [authenticate], function(req, res, next) {
+	var errorMessage = validate(req.body);
+	if (errorMessage.length > 2) {
+	  res.status(406);
+	  res.send(errorMessage);
+	}
+	else {
+	  res.locals.connection.query("UPDATE users SET ? WHERE userID=?", [req.body, requser.id], function(error, results, fields) {
+		if (error) {
+		  res.status(500);
+		  res.send(JSON.stringify({ status: 500, error: error, response: null }));
+		  //If there is error, we send the error in the error section with 500 status
+		} else {
+		  res.status(200);
+		  res.send(JSON.stringify(results));
+		  //If there is no error, all is good and response is 200OK.
+		}
+		res.locals.connection.end();
+	  });
+	}
+  });
+
 module.exports = router;
