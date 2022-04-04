30470f7b7233bb8ef20ae704761fad8c437b8268
Timothy White <timothy.white@eagles.oc.edu>
2022-04-04 15:07:29 -0500

    Get all subjects

--- routes/subjects.js
+++ routes/subjects.js
@@ -12,6 +12,21 @@ function validate(course) {
   return errorMessage;
 }
 
+router.get('/', [authenticate], function(req, res, next) {
+	res.locals.connection.query("SELECT * FROM subjects", function(error, results, fields) {
+	  if (error) {
+		res.status(500);
+		res.send(JSON.stringify({ status: 500, error: error, response: null }));
+		//If there is error, we send the error in the error section with 500 status
+	  } else {
+		res.status(200);
+		res.send(JSON.stringify(results));
+		//If there is no error, all is good and response is 200OK.
+	  }
+	  res.locals.connection.end();
+	});
+  });
+
 router.get('/:tutorID', [authenticate], function(req, res, next) {
   res.locals.connection.query("SELECT * FROM subjects s natural join tutorsubjects t where t.tutorID = ?", req.params.tutorID, function(error, results, fields) {
     if (error) {
