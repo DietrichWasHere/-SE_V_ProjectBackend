075dec55d9db12a7663bd65ef1fcc675a56e42e1
Timothy White <timothy.white@eagles.oc.edu>
2022-04-08 13:59:01 -0500

    Delete appointment request

--- routes/apptrequests.js
+++ routes/apptrequests.js
@@ -44,5 +44,20 @@ router.get('/', [authenticate, isTutor], function(req, res, next) {
 			  res.locals.connection.end();
 		  });
   });
+
+  router.delete('/:studentID/:appointmentID', [authenticate], function(req, res, next) {
+	res.locals.connection.query("DELETE FROM apptrequests WHERE studentID = ? and appointmentID = ?", [req.params.studentID, req.params.appointmentID], function(error, results, fields) {
+		if (error) {
+			res.status(500);
+			res.send(JSON.stringify({ status: 500, error: error, response: null }));
+			//If there is error, we send the error in the error section with 500 status
+		} else {
+			res.status(201);
+			res.send(JSON.stringify(results));
+			//If there is no error, all is good and response is 200OK.
+		}
+		res.locals.connection.end();
+	});
+});
   
 module.exports = router;
